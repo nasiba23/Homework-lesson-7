4b84ac29df6a9e98a6a13e7e7f28ffa2bf22a9ed
Nasiba <nasyrova.nasiba@gmail.com>
2021-02-03 19:38:06 +0500

    Solved hw

---
+++ HomeworkLesson7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HomeworkLesson7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shirt croptop = new Shirt("croptop", "blue", 40, "round", "reglan");
+        }
+    }
+    public abstract class Clothes
+    {
+        private string _id;
+        public string Title;
+        public string Color;
+        public int Size;
+
+        public Clothes (string Title, string Color, int Size)
+        {
+            this.Title = Title;
+            this.Color = Color;
+            this.Size = Size;
+        }
+    }
+    public class Shirt : Clothes
+    {
+        public string Neckline;
+        public string Sleeve;
+
+        public Shirt(string Title, string Color, int Size, string Neckline, string Sleeve) : base(Title, Color, Size)
+
+        {
+            this.Neckline = Neckline;
+            this.Sleeve = Sleeve;
+        }
+    }
+}
